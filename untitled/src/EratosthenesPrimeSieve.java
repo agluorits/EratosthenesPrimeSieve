8493d833421079e614c3d3a566d08af7620bb063
unknown <alois.gurtner84@gamil.com>
2022-09-19 19:27:40 +0200

    Prime Calculator working

--- untitled/src/EratosthenesPrimeSieve.java
+++ untitled/src/EratosthenesPrimeSieve.java
@@ -1,33 +1,53 @@
-public class EratosthenesPrimeSieve implements PrimeSieve {
-    static int max = 100;
-    static int[] array = new int[max - 1];
-    static int[] copyarray = new int[max - 1];
+import java.util.Scanner;
 
-    public static void primeCalculator() {
+public class EratosthenesPrimeSieve implements PrimeSieve {
+
+    public static void primeCalculator(int max) {
+        int[] array = new int[max - 1];
+        int[] copyarray = new int[max - 1];
         int num = 2;
         for (int i = 0; i < max - 1; i++) {
             array[i] = num;
             num++;
         }
 
-        for (int i = 0; i < max-1; i++) {
-            int currentnum = array[i];
+        for (int i = 0; i < max - 1; i++) {
             for (int j = 0; j < max - 1; j++) {
-                if (array[j] == 0) {
+                if (array[i] == 0)
+                {
                     break;
-                } else if (array[j] % currentnum == 0) {
+                } else if (array[i] == array[j])
+                {
+                } else if (array[j] % array[i] == 0)
+                {
                     array[j] = 0;
                 }
             }
         }
 
+        int counter = 0;
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            if (array[i] != 0)
+            {
+                copyarray[counter] = array[i];
+                counter++;
+            }
+        }
+
+        int[] sumarray = new int[counter];
+        for (int i = 0; i < counter; i++) {
+            sumarray[i] = copyarray[i];
+        }
+
+        for (int i = 0; i < sumarray.length; i++) {
+            System.out.println(sumarray[i]);
         }
     }
 
     public static void main(String[] args) {
-        primeCalculator();
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        primeCalculator(input);
     }
 
     @Override
